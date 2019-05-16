8d6c5f1f5578c6e9ae4aa23e0d5bb9840f76a633
RaymondAnson <616833686@qq.com>
2019-05-16 11:27:31 +0800

    测试

--- git.sh
+++ git.sh
@@ -3,5 +3,9 @@ read -p "请输入commit提交描述：" msg
 git add .
 git commit -m $msg
 git push 
+expect "Username for 'https://gitee.com'"
+send -- "443877461@qq.com\n"
+expect "PassWord for 'https://443877461@qq.com@gitee.com'"
+send -- ".dzalw962464\n"
 git status
 
