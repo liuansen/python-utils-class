fcfc824372283c38a73ed1b7caf8d1af60fb2560
liuansen <443877461@qq.com>
2019-08-05 15:39:22 +0800

    111

---
+++ to_docx/settings.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# author：Anson
+from __future__ import unicode_literals
+
+MD_PATH = 'D:/Python/Project/project/python-utils-class/to_docx/doc'
+
+SITE_1 = 4
+SITE_2 = 31
+CELL = 30
